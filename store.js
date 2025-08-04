9a1a162bf67c48ed431aeb99fc15fdfac1babb00
PAUL.s <96791484+Gretxleb@users.noreply.github.com>
2025-08-04 17:35:09 +0200

    Create store.js

---
+++ store.js
@@ -0,0 +1,76 @@
+var plugins = [
+    {
+        title: "Дополнительные плагины",
+        plugins: [
+            {
+                "url": "https://bwa.to/d",
+                "title": "Bwa.to d",
+                "descr": "Описание плагина 1"
+            },
+            {
+                "url": "http://arkmv.ru/vod",
+                "title": "Ark mv",
+                "descr": "Описание плагина 2"
+            },
+            {
+                "url": "https://nb557.github.io/plugins/online_mod.js",
+                "title": "Online Mod",
+                "descr": "Описание плагина 3"
+            },
+            {
+                "url": "http://llpp.in/v/vod.js",
+                "title": "LLPP.in vod",
+                "descr": "Описание плагина 4"
+            },
+            {
+                "url": "http://llpp.xyz/ur/all.js",
+                "title": "LLPP.xyz all",
+                "descr": "Описание плагина 5"
+            },
+            {
+                "url": "http://jin.energy/online.js",
+                "title": "Jin Energy Online",
+                "descr": "Описание плагина 6"
+            },
+            {
+                "url": "https://bwa.to/plugins/prestige.js",
+                "title": "Bwa.to Prestige",
+                "descr": "Описание плагина 7"
+            },
+            {
+                "url": "https://skaztv.online/store.js",
+                "title": "Skaztv Online",
+                "descr": "Описание плагина 8"
+            },
+            {
+                "url": "https://lampaplugins.github.io/store/store.js",
+                "title": "Lampa Plugins Store",
+                "descr": "Описание плагина 9"
+            },
+            {
+                "url": "https://raw.githubusercontent.com/lastnem/online.js-main/online.js",
+                "title": "Lastnem Online",
+                "descr": "Описание плагина 10"
+            },
+            {
+                "url": "http://xyu.govno.co.uk/online.js",
+                "title": "Xyu Govno Online",
+                "descr": "Описание плагина 11"
+            },
+            {
+                "url": "http://skaz.tv/scdn.js",
+                "title": "Skaz TV CDN",
+                "descr": "Описание плагина 12"
+            },
+            {
+                "url": "http://smotret24.com/online.js",
+                "title": "Smotret24 Online",
+                "descr": "Описание плагина 13"
+            }
+        ]
+    }
+];
+
+if (typeof start == 'function') {
+    start(plugins);
+}
